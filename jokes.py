375ff03f8de80fd0e3111064a90e6471798b32de
EhsanUllahKhan <ullahkhanihsan@yahoo.com>
2020-10-14 13:55:31 +0500

    newJokes API integrated

--- jokes.py
+++ jokes.py
@@ -24,9 +24,35 @@ def get_joke_by_category(category):
     api_end_point = "https://official-joke-api.appspot.com/jokes/"+category+"/random"
     joke = requests.get(api_end_point)
     json_data = json.loads(joke.text)
+    printDataDictionaryVise(json_data)
+
+
+def newJokeApi():
+    api_end_point = "https://api.chucknorris.io/jokes/random"
+    joke = requests.get(api_end_point)
+    json_data = json.loads(joke.text)
+    joke_category = json_data["categories"]
+    joke_created_at = json_data["created_at"]
+    joke_icon_url = json_data["icon_url"]
+    joke_id = json_data["id"]
+    joke_updated_at = json_data["updated_at"]
+    joke_url = json_data["url"]
+    joke_value = json_data["value"]
+
     print(json_data)
+    print(joke_category)
+    print(joke_created_at)
+    print(joke_icon_url)
+    print(joke_id)
+    print(joke_updated_at)
+    print(joke_url)
+    print(joke_value)
 
 
 if __name__ == "__main__":
-    get_joke()
+    # get_joke()
     # get_joke_by_category("programming")
+    newJokeApi()
+
+
+# categories , created_at , icon_url, id, updated_at ,url , value,
