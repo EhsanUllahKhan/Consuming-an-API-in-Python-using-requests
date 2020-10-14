5991913cc61858824069af706a726988c9728638
EhsanUllahKhan <ullahkhanihsan@yahoo.com>
2020-10-14 13:31:12 +0500

    Seperating values from json dictionary object

--- jokes.py
+++ jokes.py
@@ -5,7 +5,15 @@ def get_joke():
     api_end_point = "https://official-joke-api.appspot.com/jokes/random"
     joke = requests.get(api_end_point)
     json_data = json.loads(joke.text)
-    print(json_data)
+    joke_id = json_data["id"]
+    joke_type = json_data["type"]
+    joke_setup = json_data["setup"]
+    joke_punchline = json_data["punchline"]
+    print(joke_id)
+    print(joke_type)
+    print(joke_setup)
+    print(joke_punchline)
+
 
 
 if __name__ == "__main__":
